6357156bd714dd2a2d41873abfea95ce11ae1a2f
Tom Overton <tom.overton@outlook.com>
2022-05-18 16:15:06 -0700

    EnStream (vortex in Water Temple) with 1 non-matching

--- src/actors/En_Stream/z_en_stream.cpp
+++ src/actors/En_Stream/z_en_stream.cpp
@@ -8,7 +8,7 @@ void EnStream_Destroy(Actor* actor, GameState* state);
 void EnStream_Update(Actor* actor, GameState* state);
 void EnStream_Draw(Actor* actor, GameState* state);
 
-GLOBAL_ASM("binary/z_En_Stream.data.o")
+void EnStream_WaitForPlayer(EnStream* self, GlobalContext* globalCtx);
 
 ActorInit En_Stream_InitVars = {
     ACTOR_EN_STREAM,
@@ -22,11 +22,137 @@ ActorInit En_Stream_InitVars = {
     (ActorFunc)EnStream_Draw,
 };
 
-GLOBAL_ASM("binary/EnStream_Init.o")
+static InitChainEntry sInitChain[] = {
+    ICHAIN_VEC3F_DIV1000(scale, 20, ICHAIN_STOP),
+};
 
-GLOBAL_ASM("binary/EnStream_Destroy.o")
+static void EnStream_SetupAction(EnStream* self, EnStreamActionFunc actionFunc) {
+    self->actionFunc = actionFunc;
+}
 
-GLOBAL_ASM("binary/EnStream_Update.o")
+void EnStream_Init(Actor* actor, GameState* state) {
+    EnStream* self = (EnStream*)actor;
+    GlobalContext* globalCtx = (GlobalContext*)state;
 
-GLOBAL_ASM("binary/EnStream_Draw.o")
+    self->unk_1A8 = self->actor.params & 0xFF;
+    ZARInfo* zarInfo = FUN_00372f38(actor, globalCtx, &self->skelAnimModel, 0, 0);
+    self->skelAnimModel->unk_0C->FUN_00372d94(zarInfo->GetCMABByIndex(0));
+    self->skelAnimModel->unk_0C->SetAnimType(1);
+    self->skelAnimModel->unk_0C->SetAnimSpeed(2.0f);
+    Actor_ProcessInitChain(actor, sInitChain);
+    if (self->unk_1A8 == 1) {
+        actor->scale.y = 0.01f;
+    }
+
+    EnStream_SetupAction(self, EnStream_WaitForPlayer);
+}
+
+void EnStream_Destroy(Actor* actor, GameState* state) {
+    EnStream* self = (EnStream*)actor;
+
+    FUN_00350f34(&self->actor, &self->skelAnimModel, 0);
+}
+
+static s32 EnStream_PlayerIsInRange(Vec3f* vortexWorldPos, Vec3f* playerWorldPos, Vec3f* posDifference, f32 vortexYScale) {
+    f32 upperBounds = 160 * vortexYScale * 50.0f;
+    f32 lowerBounds = 0 * vortexYScale * 50.0f;
+    f32 smallConstant;
+    f32 xzDist;
+    f32 range;
+    s32 ret = 0;
+
+    smallConstant = 28.0f;
+    
+    posDifference->x = playerWorldPos->x - vortexWorldPos->x;
+    posDifference->y = playerWorldPos->y - vortexWorldPos->y;
+    posDifference->z = playerWorldPos->z - vortexWorldPos->z;
+    xzDist = sqrtf(SQ(posDifference->x) + SQ(posDifference->z));
+
+    if (lowerBounds <= posDifference->y && posDifference->y <= upperBounds) {
+        posDifference->y -= lowerBounds;
+
+        range = ((75.0f - smallConstant) * (posDifference->y / (upperBounds - lowerBounds))) + smallConstant;
+        if (xzDist <= range) {
+            ret = 1;
+        }
+    }
+
+    if ((posDifference->y <= lowerBounds) && (xzDist <= smallConstant)) {
+        ret = 2;
+    }
+
+    return ret;
+}
+
+#ifdef NON_MATCHING
+void EnStream_SuckPlayer(EnStream* self, GlobalContext* globalCtx) {
+    Player* player = GET_PLAYER(globalCtx);
+    Actor* actor = &self->actor;
+    Vec3f posDifference;
+    f32 xzDist;
+    f32 yDistWithOffset;
+    f32 temp;
+    s32 ret = EnStream_PlayerIsInRange(&actor->world.pos, &player->actor.world.pos, &posDifference, actor->scale.y);
+
+    if (ret != 0) {
+        xzDist = sqrtf(SQ(posDifference.x) + SQ(posDifference.z));
+        yDistWithOffset = player->actor.world.pos.y - (actor->world.pos.y - 90.0f);
+        temp = Math_FAtan2F(-posDifference.x, -posDifference.z) * (0x8000 / M_PI);
+        player->pushedYaw = temp;
+        if (xzDist > 3.0f) {
+            Math_SmoothStepToF(&player->pushedSpeed, 3.0f, 0.5f, xzDist, 0.0f);
+        } else {
+            player->pushedSpeed = 0.0f;
+            Math_SmoothStepToF(&player->actor.world.pos.x, actor->world.pos.x, 0.5f, 3.0f, 0.0f);
+            Math_SmoothStepToF(&player->actor.world.pos.z, actor->world.pos.z, 0.5f, 3.0f, 0.0f);
+        }
+
+        if (yDistWithOffset > 0.0f) {
+            Math_SmoothStepToF(&player->actor.velocity.y, -3.0f, 0.7f, yDistWithOffset, 0.0f);
+            if (posDifference.y < -70.0f) {
+                player->stateFlags2 |= 0x80000000;
+            }
+        }
+    } else {
+        EnStream_SetupAction(self, EnStream_WaitForPlayer);
+    }
+}
+#else
+void EnStream_SuckPlayer(EnStream* self, GlobalContext* globalCtx);
+GLOBAL_ASM("binary/EnStream_SuckPlayer.o")
+#endif
+
+void EnStream_WaitForPlayer(EnStream* self, GlobalContext* globalCtx) {
+    Player* player = GET_PLAYER(globalCtx);
+    Vec3f temp;
+
+    if (EnStream_PlayerIsInRange(&self->actor.world.pos, &player->actor.world.pos, &temp, self->actor.scale.y) != 0) {
+        EnStream_SetupAction(self, EnStream_SuckPlayer);
+    }
+}
+
+void EnStream_Update(Actor* actor, GameState* state) {
+    EnStream* self = (EnStream*)actor;
+    GlobalContext* globalCtx = (GlobalContext*)state;
+
+    self->actionFunc(self, globalCtx);
+    FUN_0035ae08(&self->actor, 0x1000234);
+}
+
+void EnStream_Draw(Actor* actor, GameState* state) {
+    EnStream* self = (EnStream*)actor;
+    MTX34 modelMtx;
+
+    MTX34CopyAsm(&modelMtx, &self->actor.modelMtx);
+
+    f32 animSpeed = 2.0f;
+    if (PauseContext_GetState() != 0) {
+        animSpeed = 0.0f;
+    }
+
+    self->skelAnimModel->unk_0C->SetAnimSpeed(animSpeed);
+    self->skelAnimModel->SetUnkAC(1);
+    self->skelAnimModel->FUN_003721e0(&modelMtx);
+    self->skelAnimModel->FUN_00372170(0);
+}
 }
